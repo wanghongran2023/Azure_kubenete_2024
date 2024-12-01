25687102384f5cb4edc5ef6b1e8f14ebe819f547
Ubuntu <ubuntu@ip-192-168-55-188.ap-northeast-1.compute.internal>
2024-12-01 08:20:58 +0000

    fix

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -91,10 +91,9 @@ resource "azurerm_linux_function_app" "function" {
   name                = var.function_config.name
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  app_service_plan_id = azurerm_service_plan.service_plan.id
+  service_plan_id = azurerm_service_plan.service_plan.id
   storage_account_name = azurerm_storage_account.storage_account.name
   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
-  os_type             = "linux"
 
   site_config {
     application_stack {
