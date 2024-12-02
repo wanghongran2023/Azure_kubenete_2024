bb3338c2e159712fca6709af72a6c7d436e7843b
wanghongran2023 <147599434+wanghongran2023@users.noreply.github.com>
2024-12-02 12:09:22 +0900

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -116,3 +116,44 @@ output "cosmos_db_connection_string" {
   value     = azurerm_cosmosdb_account.cosmos_account.connection_strings[0]
   sensitive = true
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_config.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Create AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_config.name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  dns_prefix          = "exampleaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr_permission" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
